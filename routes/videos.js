0584c7c4551e9463d68852fb75f6569f2402a438
Unique Dismuke <uniquedismuke@Uniques-MacBook-Air.local>
2023-11-01 18:07:58 -0400

    add to vidoes.js and index.js

---
+++ routes/videos.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const fs = require("fs");
+const { v4: uuid } = require("uuid");
+const router = express.Router();
+// Get the data from the posts JSON file
+const videos = fs.readFileSync("./data/videos.json");
+// Parse it from JSON into JavaScript
+const parsedVideos = JSON.parse(videos);
+// This endpoint will repsond to a GET request to `/posts`
+router.get("/", (_req, res) => {
+  res.json(parsedVideos);
+});
+// editted
+router.get("/:id", (req, res) => {
+    const foundVideo = parsedVideos.find(video => video.id === req.params.id)
+    res.json(foundVideo);
+  });
