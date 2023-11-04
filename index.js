9c5d79694dce7b20925b08332a9df30270630a76
Unique Dismuke <uniquedismuke@Uniques-MacBook-Air.local>
2023-11-03 23:26:36 -0400

    im not sure

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// Allow JSON to be sent/recieved
+
 app.use(express.json());
-// Allow the `public` directory to serve static files
+
 app.use(express.static("public"));
-// Allow cross origin for a particular origin (our frontend), but block requests from any other origin
+
 app.use(cors());
-// Import the post routes and register them using the base endpoint of `/posts` (any route inside the route fill will start with `/posts`)
+
 const postVideos = require("./routes/videos");
 app.use("/videos", postVideos);
 app.listen(8080, () => {
